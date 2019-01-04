ba86769ee04c79460c49566eb9714aa2626a5808
baiye <1159428886@qq.com>
2019-01-04 21:23:40 +0800

    创建试图login

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResopnse
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResopnse('index')
+
+def login(request):
+    return redirect('/index')
